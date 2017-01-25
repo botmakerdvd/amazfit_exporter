77ec3d765d22ebeda159190df12da889cf760406
dvd_ath <botmakerdvd@gmail.com>
2017-01-25 19:07:40 +0100

    Initial version

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # amazfit_exporter
+This python script helps you to export your huami Amazfit activities to other platforms like Strava,Runtastic...
+At current version the script has to be used like this
+python3 amazfit_exporter.py sport_data.db
+
+The sport_data.db file has to be get from the Amazfit by ADB, there are two methods for this
+- connect amazfit to pc
+- adb backup -f /export_data.ab -noapk com.huami.watch.sport
+- java -jar abe.jar unpack export_data.ab export_data.tar (abe is android backup extractor which is included inside tools folder)
+- extract the tar file using winrar
+- navigate to export_data\apps\com.huami.watch.sport\db folder and copy sport_data.db
+
+
+or if you have a rooted rom , just execute command
+adb pull /data/data/com.huami.watch.sport/databases/sport_data.db
+
+#CHANGELOG
+-V1.0 generates .gpx file for each activity
---
+++ src/amazfit_exporter.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+import sqlite3 as lite
+import sys
+import datetime
+import os
+con = lite.connect(sys.argv[1])
+
+with con:
+    
+	cur = con.cursor()    
+	cur.execute('SELECT track_id, start_time from sport_summary where type=2')
+	running_sessions = cur.fetchall()
+	for running_session in running_sessions:
+		track_id=running_session[0]
+		tiempo_init=running_session[1]
+		cur.execute('SELECT location_data.latitude, location_data.longitude, location_data.altitude, location_data.timestamp from location_data, sport_summary where location_data.track_id=' + str(track_id) + ' and sport_summary.type=2 ')
+		datos = cur.fetchall()
+		year=datetime.datetime.fromtimestamp(running_session[1]/1000).strftime('%Y')
+		month=datetime.datetime.fromtimestamp(running_session[1]/1000).strftime('%m')
+		day=datetime.datetime.fromtimestamp(running_session[1]/1000).strftime('%d')
+		hour=datetime.datetime.fromtimestamp(running_session[1]/1000).strftime('%H')
+		minute=datetime.datetime.fromtimestamp(running_session[1]/1000).strftime('%M')
+		second=datetime.datetime.fromtimestamp(running_session[1]/1000).strftime('%S')
+		try:
+			os.remove(year+month+day+'_'+hour+minute+second+'.gpx')
+		except OSError:
+			pass
+		with open(year+month+day+'_'+hour+minute+second+'.gpx', 'a') as out:	
+			out.write('<?xml version="1.0" encoding="UTF-8"?>' + '\r\n')
+			out.write('<gpx version="1.1" creator="Amazfit_export by dvd_ath" xsi:schemaLocation="http://www.topografix.com/GPX/1/1' + '\r\n')
+			out.write('                                 http://www.topografix.com/GPX/1/1/gpx.xsd' + '\r\n')
+			out.write('                                 http://www.garmin.com/xmlschemas/GpxExtensions/v3' + '\r\n')
+			out.write('                                 http://www.garmin.com/xmlschemas/GpxExtensionsv3.xsd' + '\r\n')
+			out.write('		                            http://www.garmin.com/xmlschemas/TrackPointExtension/v1' + '\r\n')
+			out.write('                                 http://www.garmin.com/xmlschemas/TrackPointExtensionv1.xsd" xmlns="http://www.topografix.com/GPX/1/1" xmlns:gpxtpx="http://www.garmin.com/xmlschemas/TrackPointExtension/v1" xmlns:gpxx="http://www.garmin.com/xmlschemas/GpxExtensions/v3" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">' + '\r\n')
+			out.write(' <metadata>'  + '\r\n')
+			out.write('  <link href="https://github.com/botmakerdvd/amazfit_exporter">'+ '\r\n')
+			out.write('    <text>Amazfit exporter</text>'+ '\r\n')
+			out.write('  </link>'+ '\r\n')
+			out.write('  <time>'+year+'-'+month+'-'+day+'T'+hour+':'+minute+':'+second+'.000Z</time>'+ '\r\n')
+			out.write(' </metadata>'+ '\r\n')
+			out.write(' <trk>'+ '\r\n')
+			out.write('  <trkseg>'+ '\r\n')
+			for dato in datos:
+				latitud=str(dato[0])
+				longitud=str(dato[1])
+				altitud=str(round(dato[2],1))
+				time=((dato[3]+tiempo_init)/1000)
+				year=datetime.datetime.fromtimestamp(time).strftime('%Y')
+				month=datetime.datetime.fromtimestamp(time).strftime('%m')
+				day=datetime.datetime.fromtimestamp(time).strftime('%d')
+				hour=datetime.datetime.fromtimestamp(time).strftime('%H')
+				minute=datetime.datetime.fromtimestamp(time).strftime('%M')
+				second=datetime.datetime.fromtimestamp(time).strftime('%S')			
+				cur.execute('SELECT rate from heart_rate where time=' + str(time*1000))
+				rate=cur.fetchone()
+				out.write('   <trkpt lon="'+longitud+'" lat="'+latitud+'">'+ '\r\n')
+				out.write('    <ele>'+altitud+'</ele>'+ '\r\n')
+				out.write('    <time>'+year+'-'+month+'-'+day+'T'+hour+':'+minute+':'+second+'.000Z</time>'+ '\r\n')
+				if rate is not None:
+					out.write('    <extensions>'+ '\r\n')
+					out.write('     <gpxtpx:TrackPointExtension>'+ '\r\n')
+					out.write(' 	 <gpxtpx:hr>'+str(int(rate[0]))+'</gpxtpx:hr>'+ '\r\n')
+					out.write('     </gpxtpx:TrackPointExtension>'+ '\r\n')
+					out.write('    </extensions>'+ '\r\n')
+				out.write('   </trkpt>'+ '\r\n')
+			out.write('  </trkseg>'+ '\r\n')
+			out.write(' </trk>'+ '\r\n')
+			out.write('</gpx>'+ '\r\n')
+			out.close()
