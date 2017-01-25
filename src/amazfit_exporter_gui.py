eaa5a03f68da2068a8109aee24a44330d5e8d9e1
dvd_ath <botmakerdvd@gmail.com>
2017-01-25 21:48:29 +0100

    updated to PyQt5

--- src/amazfit_exporter_gui.py
+++ src/amazfit_exporter_gui.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 import sys
-from PyQt4 import QtCore, QtGui, uic
-from PyQt4.QtGui import QFileDialog
+import amazfit_exporter
+from PyQt5 import QtCore, QtWidgets, uic
+from PyQt5.QtWidgets import QFileDialog
 
 form_class = uic.loadUiType("amazfit_exporter_gui.ui")[0]
-class MyWindowClass(QtGui.QMainWindow, form_class):
+class MyWindowClass(QtWidgets.QMainWindow, form_class):
 	def __init__(self, parent=None):
-		QtGui.QMainWindow.__init__(self, parent)
+		QtWidgets.QMainWindow.__init__(self, parent)
 		self.setupUi(self)
 		self.but_select_orig.clicked.connect(self.but_select_orig_clicked)
 		self.but_select_dest.clicked.connect(self.but_select_dest_clicked)
 		self.but_generate.clicked.connect(self.gpx_fromdb)
 
 	def but_select_orig_clicked(self):
-		self.text_path_orig.setText(QFileDialog.getOpenFileName())
+		self.text_path_orig.setText(QFileDialog.getOpenFileName()[0])
 	def but_select_dest_clicked(self):
 		self.text_path_dest.setText(QFileDialog.getExistingDirectory())
 	def gpx_fromdb(self):
@@ -21,7 +22,7 @@ class MyWindowClass(QtGui.QMainWindow, form_class):
 		dest = self.text_path_dest.text()
 		amazfit_exporter.db_to_gpx(db,dest)
 		self.endlabel.setText("GPX files exportation finished")
-app = QtGui.QApplication(sys.argv)
+app = QtWidgets.QApplication(sys.argv)
 MyWindow = MyWindowClass(None)
 MyWindow.show()
 app.exec_()
